09e8409f382e65e86e40f201f07c9245295f55ae
szalonahulia <juliaobarska@interia.pl>
2018-09-11 12:21:06 +0200

    może jednak biol chem...

---
+++ python/szablon.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  szablon.py
+#  
+
+
+
+def main(args):
+    print("ELO PSYS")
+    return 0
+
+if __name__ == '__main__':
+    import
+    sys.exit(main(sys.argv))
+
