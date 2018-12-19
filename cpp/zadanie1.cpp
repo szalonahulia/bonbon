996722d9d6a76e99d66de2cb7a52cc4f5747cd6c
szalonahulia <juliaobarska@interia.pl>
2018-12-19 08:45:36 +0100

    Zadanka na lekcju

---
+++ cpp/zadanie1.cpp
@@ -0,0 +1,32 @@
+/*
+ * zadanie1.cpp
+ * 
+ 
+ */
+
+
+#include <iostream>
+using namespace std;
+void cw01(liczba , wynik) {
+    int liczba =0;
+    int wynik =0;
+    while (wynik <= 75) {
+        
+        cout << "podaj liczbe: ";
+        cin >> liczba;
+        wynik = wynik + liczba;
+    }   
+    cout << wynik; 
+}
+void cw02() {
+    int m = 0;
+    int n = 0;
+    for(int i = n; i <= m; i++)
+    cout << " " << i << " ";
+    
+int main(int argc, char **argv)
+{   
+	
+	return 0;
+}
+
